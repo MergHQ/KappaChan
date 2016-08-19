80f0dfa0b8d490c50c4a98815060e7de086e4c23
MergHQ <hugo.holm@live.com>
2016-08-19 12:19:32 +0300

    Reconnect to WS on close

--- src/logger.js
+++ src/logger.js
@@ -2,21 +2,30 @@
 
 module.exports = function () {
   const WebSocket = require('ws');
-  var ws = new WebSocket(App.config.logserverIP, {headers: {'token': App.config.token}});
+  var ws = null;
+  connect();
   var self = this;
 
-  ws.on('error', (e) => {
-    console.log(e);
-  });
+  function connect() {
+    ws = new WebSocket(App.config.logserverIP, {headers: {'token': App.config.token}});
+    ws.on('error', (e) => {
+      console.log(e);
+    });
 
-  ws.on('open', () => {
-    self.log = (payload, logLevel) => {
-      var obj = {
-        type: logLevel,
-        payload: payload
+    ws.on('close', () => {
+      // Attempt reconnects
+      setTimeout(connect, 5000);
+    });
+
+    ws.on('open', () => {
+      self.log = (payload, logLevel) => {
+        var obj = {
+          type: logLevel,
+          payload: payload
+        };
+
+        ws.send(JSON.stringify(obj));
       };
-
-      ws.send(JSON.stringify(obj));
-    };
-  });
+    });
+  }
 };
