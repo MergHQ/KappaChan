87bcd260843fbc48916b28a1b0ad08310257b8e4
MergHQ <hugo.holm@live.com>
2017-02-04 00:18:42 +0200

    Fix faulty endpoint

--- src/api/twitch.js
+++ src/api/twitch.js
@@ -1,6 +1,6 @@
 module.exports = {
   GET_emoteList: 'https://api.twitch.tv/kraken/chat/emoticons',
-  GET_channel: (chan) => { return 'https://api.twitch.tv/kraken/streams?channel=' + chan; },
+  GET_channel: (chan) => { return 'https://api.twitch.tv/kraken/streams?' + chan; },
   GET_streamSearch: (q) => { return 'https://api.twitch.tv/kraken/search/streams?limit=10&q=' + q; },
   GET_channelSearch: (q) => { return 'https://api.twitch.tv/kraken/search/channels?limit=10&q=' + q; }
 };
